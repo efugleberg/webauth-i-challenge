768263b460f96d5fcf8e940124f503c7f9e23bc6
Eric Fugleberg <ericfugleberg@Erics-MacBook-Pro.local>
2019-07-22 16:12:34 -0500

    WIP: Working thru router functions

---
+++ API/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const UsersRouter = require("../users/users-router.js");
+const AuthRouter = require("../authenticate/auth-router.js");
+
+const server = express();
+
+server.use(express.join());
+
+server.use("/api/users", UsersRouter);
+server.use("/api", AuthRouter);
+
+module.exports = server;
---
+++ authenticate/auth-middleware.js
@@ -0,0 +1,24 @@
+const db = require("../database/dbConfig.js");
+const Users = require("../users/users-model.js");
+const bcrypt = require("bcryptjs");
+
+module.exports = function authenticate(req, res, next) {
+  const { username, password } = req.headers;
+
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "invalid credentials" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: "unexpected error" });
+      });
+  } else {
+    res.status(400).json({ message: "you shall not pass!" });
+  }
+};
